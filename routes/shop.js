7fd478898cb73be4cd72e35f55c0276c7782f788
ScriptsGuy <teachingscripts@gmail.com>
2018-12-18 17:09:25 +0100

    reboot the project

---
+++ routes/shop.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const { products } = adminData;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/'
+  });
+});
+
+module.exports = router;
