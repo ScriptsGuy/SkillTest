fc4ca20201440f47ba02cf701f7a34aab7b0aba6
ScriptsGuy <teachingscripts@gmail.com>
2018-12-19 20:00:23 +0100

    cars and computers forms are working

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,6 +6,12 @@ exports.getAddComputersAccessories = (req, res, next) => {
     path: '/admin/ComputersAccessories'
   });
 };
+exports.getAddCars = (req, res, next) => {
+  res.render('admin/cars', {
+    pageTitle: 'Add Product',
+    path: '/admin/cars'
+  });
+};
 
 exports.postAddProduct = (req, res, next) => {
   const { name } = req.body;
@@ -27,6 +33,31 @@ exports.postAddProduct = (req, res, next) => {
   const { description } = req.body;
   const product = new Product(name, category, image, description, price);
   console.log('NEW Product Added'.bgCyan.black);
+  product.save();
+  res.redirect('/');
+};
+exports.postAddCar = (req, res, next) => {
+  const { name } = req.body;
+  const { year } = req.body;
+  const { model } = req.body;
+  const { version } = req.body;
+  const { category } = req.body;
+  const { engin } = req.body;
+  const { energy } = req.body;
+  const { gearBox } = req.body;
+  const { color } = req.body;
+  const { milage } = req.body;
+  const { papers } = req.body;
+  const { price } = req.body;
+  const { priceState } = req.body;
+  const { exchange } = req.body;
+  const { image } = req.body;
+  const { city } = req.body;
+  const { phoneNumber } = req.body;
+  const { email } = req.body;
+  const { description } = req.body;
+  const product = new Product(name, category, image, description, price);
+  console.log('NEW car Added'.bgCyan.black);
   product.save();
   res.redirect('/');
 };
--- routes/admin.js
+++ routes/admin.js
@@ -6,10 +6,12 @@ const adminContoller = require('../controllers/admin');
 
 // /admin/add-product => GET
 router.get('/ComputersAccessories', adminContoller.getAddComputersAccessories);
+router.get('/cars', adminContoller.getAddCars);
 // this get is for admin products
 router.get('/products', adminContoller.getProducts);
 
 // /admin/add-product => POST
 router.post('/ComputersAccessories', adminContoller.postAddProduct);
+router.post('/cars', adminContoller.postAddCar);
 
 module.exports = router;
